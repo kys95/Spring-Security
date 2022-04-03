33351c4385e36965db6e2249f2568d84c9eab3be
Yunseok Kang <mok02198@gmail.com>
2022-04-03 17:24:20 +0900

    Init: 프로젝트 생성

---
+++ src/main/java/com/security/kys95/Kys95Application.java
@@ -0,0 +1,13 @@
+package com.security.kys95;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kys95Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kys95Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/kys95/Kys95ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.kys95;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kys95ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
