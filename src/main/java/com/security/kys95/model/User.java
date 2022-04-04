f9ace0ee4b5fd5c0912f9751eab492beae10c070
Yunseok Kang <mok02198@gmail.com>
2022-04-04 15:11:32 +0900

    Create User.java

---
+++ src/main/java/com/security/kys95/model/User.java
@@ -0,0 +1,27 @@
+package com.security.kys95.model;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@Data
+@Entity
+public class User {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;    //ROLE_USER, ROLE_ADMIN
+
+    @CreationTimestamp
+    private Timestamp createDate;
+
+}
