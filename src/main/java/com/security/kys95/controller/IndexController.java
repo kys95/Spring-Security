88f15884d3885f3ee627b09ed21d700fb149a9c8
Yunseok Kang <mok02198@gmail.com>
2022-04-04 14:36:42 +0900

    Update IndexController.java

--- src/main/java/com/security/kys95/controller/IndexController.java
+++ src/main/java/com/security/kys95/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.security.kys95.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //return view
@@ -10,6 +11,36 @@ public class IndexController {
     @GetMapping({"", "/"})  //localhost:8080/ or localhost:8080
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user(){
+        return "user";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/login")
+    public @ResponseBody String login(){
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join(){
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc(){
+        return "join success";
     }
 
 }
