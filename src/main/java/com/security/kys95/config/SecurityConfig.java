4d23077d5f8026b319d110049c16943ef4c5224a
Yunseok Kang <mok02198@gmail.com>
2022-04-04 15:12:12 +0900

    Update SecurityConfig.java

--- src/main/java/com/security/kys95/config/SecurityConfig.java
+++ src/main/java/com/security/kys95/config/SecurityConfig.java
@@ -1,13 +1,21 @@
 package com.security.kys95.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //해당 메서드의 return되는 오브젝트를 IoC로 등록해줌
+   @Bean
+    public BCryptPasswordEncoder encodePwd(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
-                .loginPage("/login");
+                .loginPage("/loginForm");
 
     }
 }
