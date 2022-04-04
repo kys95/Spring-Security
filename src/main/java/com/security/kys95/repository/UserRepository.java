158418857d028c9f54c250a777016621b074bc24
Yunseok Kang <mok02198@gmail.com>
2022-04-04 16:25:39 +0900

    Update UserRepository.java

--- src/main/java/com/security/kys95/repository/UserRepository.java
+++ src/main/java/com/security/kys95/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.security.kys95.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    public User findByUsername(String username);
 }
