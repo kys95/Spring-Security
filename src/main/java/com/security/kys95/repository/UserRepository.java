a78e11342145b81d1979c42ec389ed109e277d44
Yunseok Kang <mok02198@gmail.com>
2022-04-04 15:58:18 +0900

    Refactor: UserRepository 스펠링 수정

--- src/main/java/com/security/kys95/repository/UserRepository.java
+++ src/main/java/com/security/kys95/repository/UserRepository.java
@@ -3,5 +3,6 @@ package com.security.kys95.repository;
 import com.security.kys95.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepostiory extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
+
 }
