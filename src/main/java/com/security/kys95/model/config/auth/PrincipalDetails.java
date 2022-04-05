276c022d75cdc8a5ed86df7433f0d96e1ebfe37f
Yunseok Kang <mok02198@gmail.com>
2022-04-05 13:41:43 +0900

    Update PrincipalDetails.java

--- src/main/java/com/security/kys95/model/config/auth/PrincipalDetails.java
+++ src/main/java/com/security/kys95/model/config/auth/PrincipalDetails.java
@@ -1,22 +1,34 @@
 package com.security.kys95.model.config.auth;
 
 import com.security.kys95.model.User;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 /**
  * Security Session에 session 정보를 저장 -> 들어갈 수 있는 객체는 Authentication type
  * Authentication 객체 안에 User정보를 저장 -> 들어갈 수 있는 객체는 UserDetails type(PrincipalDetails type)
  */
-public class PrincipalDetails implements UserDetails {
+@Data
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private User user;  //composition
+    private Map<String, Object> attributes;
 
+    //not OAuth Login
     public PrincipalDetails(User user) {
         this.user = user;
+    }
+
+    //OAuth Login
+    public PrincipalDetails(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
     }
 
     //해당 user의 권한 return
@@ -64,5 +76,15 @@ public class PrincipalDetails implements UserDetails {
         //ex)회원이 1년동안 로그인을 안하면 휴먼 계정으로 돌림
         //if user의 현재시간-로그인시간 > 1년, return false;
         return true;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getName() {
+        return null;
     }
 }
