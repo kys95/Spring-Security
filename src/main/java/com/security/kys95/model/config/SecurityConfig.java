c9f10dc7e6fcf271a18c358334575d511d48d333
Yunseok Kang <mok02198@gmail.com>
2022-04-05 12:28:19 +0900

    Update SecurityConfig.java

--- src/main/java/com/security/kys95/model/config/SecurityConfig.java
+++ src/main/java/com/security/kys95/model/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.security.kys95.model.config;
 
+import com.security.kys95.model.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,10 +10,20 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+/**
+ * 1.코드받기(인증)
+ * 2.access token(권한)
+ * 3.사용자프로필 정보를 가져옴
+ * 4-1.그 정보를 토대로 회원가입을 자동으로 진행시킴
+ * 4-2.구글(이메일,전화번호,이름,아이디), 쇼핑몰(집주소,등급)
+ */
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)  //@Secured 활성화, @PreAuthorize & @PostAuthorize 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
 
     //해당 메서드의 return되는 오브젝트를 IoC로 등록해줌
    @Bean
@@ -34,7 +46,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/")
                 .and()
                 .oauth2Login()
-                .loginPage("/loginForm");
+                .loginPage("/loginForm") //구글 로그인 완료후 후처리 필요.(access token,사용자 프로필 정보 받음)
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService);
 
     }
 }
