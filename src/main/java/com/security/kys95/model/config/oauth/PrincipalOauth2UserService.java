83b11219263e2e45ac773fc7cc6eef99c3fb8559
Yunseok Kang <mok02198@gmail.com>
2022-04-05 13:41:54 +0900

    Update PrincipalOauth2UserService.java

--- src/main/java/com/security/kys95/model/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/security/kys95/model/config/oauth/PrincipalOauth2UserService.java
@@ -12,7 +12,11 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
     //google로부터 받은 userRequest data에 대한 후처리되는 함수
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        //google login 버튼->google login 완료->code return(Oauth-Client 라이브러리)->access token요청
+        //userRequest정보->loadUser함수 호출->google로부터 회원프로필 받아줌
         System.out.println(super.loadUser(userRequest).getAttributes());
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
         return super.loadUser(userRequest);
     }
 }
